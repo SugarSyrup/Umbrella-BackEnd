d283eadfc7f158fbdcff85feba1ecd54d547a51b
SugarSyrup <tlfvm04@naver.com>
2023-01-23 19:49:12 +0900

    FE <-> BE 통신 테스트

--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
 import express from 'express';
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => {
-    res.render("home");
-})
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.send("hello!!!");
+});
 
 const handleListen = () => {
     console.log(`http://localhost:3000 is now running`);
+    console.log(`ws://localhost:3000 is now running`);
 }
-app.listen(3000, handleListen);
+server.listen(3000, handleListen);
+
+
